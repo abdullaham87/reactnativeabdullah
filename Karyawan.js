f557594bb1d30b4f6d13a1496ffd9917c66c3ea4
abudullaham87 <93036532+abudullaham87@users.noreply.github.com>
2022-11-05 22:59:16 +0700

    Add files via upload

--- Karyawan.js
+++ Karyawan.js
@@ -1,9 +1,9 @@
 const karyawan = [
     {id:'1', nama:'Abdullah Arwani M', jabatan:'Owner', email:'aamarema36@gmail.com',
-    telp:'085815151264', gaji:'20.000.000', foto:'https://placeimg.com/100/100/nature'},
+    telp:'085815151264', gaji:'20.000.000', foto:'https://placeimg.com/100/100/image.png'},
 
     {id:'2', nama:'Jackson', jabatan:'Manager', email:'jacsontiago@yahoo.co.id',
-    telp:'089389272', gaji:'100.000.000', foto:'https://placeimg.com/100/100/any'},
+    telp:'089389272', gaji:'100.000.000', foto:'https://image.png.com/100/100/any'},
 
     {id:'3', nama:'Linus Torvalds', jabatan:'Direktur', email:'linus@gmail.com',
     telp:'08872973927', gaji:'100.000.000', foto:'https://placeimg.com/100/100/tech'},
