fc63e8521618681201616b07897bafbff0bee7bf
abudullaham87 <93036532+abudullaham87@users.noreply.github.com>
2022-11-14 13:48:25 +0700

    Update Details.js

--- navigation/Details.js
+++ navigation/Details.js
@@ -2,16 +2,19 @@ import * as React from 'react';
 import { View, Text, StyleSheet, Button, Image, Linking} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {Title, Card} from 'react-native-paper';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import Entypo from 'react-native-vector-icons/Entypo';
+import email from '../icon/email.png' ;
+import call from '../icon/call.png' ;
+import dollar from '../icon/dollar.png' ;
 
 const Details = ({navigation, route}) => {
     const {karyawan} = route.params
+    
     return (
         <View style={{flex:1}}>
             <LinearGradient colors={['#0033ff','#6bc1ff']} style={{height:'20%'}}/>
             <View style={{alignItems:'center'}}>
-                <Image style={{width:120, height:120, borderRadius:120/2, marginTop:-50}}
+                <Image style={{width:120, height:120, borderRadius:120/2,
+                marginTop:-50}}
                 source={{uri:karyawan.foto}}/>
             </View>
 
@@ -19,31 +22,26 @@ const Details = ({navigation, route}) => {
                 <Title>{karyawan.nama}</Title>
                 <Text style={{fontSize:14}}>{karyawan.jabatan}</Text>
             </View>
-
             <Card style={{margin:3}}
-            onPress={()=>{Linking.openURL('mailto:${karyawan.email})')}}>
+            onPress={()=>{Linking.openURL('mailto:${karyawan.email}&subject')}}>
                 <View style={{flexDirection:'row', padding:8}}>
-                <MaterialIcons name='email' size={32} color='#006aff'/>
+                <Image source={email} style={{width:30, height:30}} />
                 <Text style={styles.teks}>{karyawan.email}</Text>
                 </View>
             </Card>
-
             <Card style={{margin:3}}
             onPress={()=>{Linking.openURL('tel:${karyawan.telp}')}}>
                 <View style={{flexDirection:'row', padding:8}}>
-                    <Entypo name="email" size={32} color='#006aff'/>
+                    <Image source={call} style={{width:30, height:30}}/>
                     <Text style={styles.teks}>{karyawan.telp}</Text>
                 </View>
             </Card>
-
-            <Card style={{margin:3}}
-            onPress={()=>{Linking.openURL('tel:${karyawan.gaji}')}}>
+            <Card style={{margin:3}}>
                 <View style={{flexDirection:'row', padding:8}}>
-                    <MaterialIcons name="attach-money" size={32} color='#006aff'/>
+                    <Image source={dollar} style={{width:30, height:30}}/>
                     <Text style={styles.teks}>{karyawan.gaji}</Text>
                 </View>
             </Card>
-
             <View style={{padding:10}}>
                 <Button title='GO BACK' onPress={()=>navigation.navigate('Home')}/>
             </View>
